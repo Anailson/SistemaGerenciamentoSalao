903e50defe414a8ae5117546bebe6dd0ff0ec22c
Anailson Ribeiro <arsantos315@gmail.com>
2020-07-15 18:11:02 -0300

    Projeto Salao

---
+++ src/main/java/br/com/sistema/SistemaGerenciamentoSalaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoSalaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoSalaoApplication.class, args);
+	}
+
+}
