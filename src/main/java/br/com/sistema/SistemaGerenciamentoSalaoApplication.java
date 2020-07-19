3a3097540dbac827818cec912c1e84ef14ff6774
Anailson Ribeiro <arsantos315@gmail.com>
2020-07-19 18:24:49 -0300

    CadastroCliente

--- src/main/java/br/com/sistema/SistemaGerenciamentoSalaoApplication.java
+++ src/main/java/br/com/sistema/SistemaGerenciamentoSalaoApplication.java
@@ -3,9 +3,11 @@ package br.com.sistema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.sistema.model")
+@ComponentScan(basePackages = {"*br.*"})
 public class SistemaGerenciamentoSalaoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/sistema/controller/ClienteController.java
+++ src/main/java/br/com/sistema/controller/ClienteController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.sistema.model.Cliente;
 import br.com.sistema.repository.ClienteRepository;
 
 @Controller
@@ -19,5 +21,30 @@ public class ClienteController {
 		
 		return "cadastro/cadastrocliente";
 	}
+	
+	//CADASTRAR CLIENTE
+	@RequestMapping(method = RequestMethod.POST, value = "/salvarcliente")
+	public ModelAndView salvar(Cliente cliente) {
+		clienteRepository.save(cliente);
+		
+		/*Salvando os dados na lista de clientes*/
+		ModelAndView andView = new ModelAndView("cadastro/cadastrocliente");
+		Iterable<Cliente> clienteIt = clienteRepository.findAll();
+		andView.addObject("clientes", clienteIt);
+		
+		return andView;
+	}
+	
+	//LISTRAR CLIENTES
+	@RequestMapping(method = RequestMethod.GET, value = "/listaclientes")
+	public ModelAndView clientes() {
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastrocliente");
+		Iterable<Cliente> clienteIt = clienteRepository.findAll();
+		andView.addObject("clientes", clienteIt);
+		
+		return andView;
+	}
+	
 
 }
